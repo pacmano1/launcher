f3e8185f5afc649bb9c66dfe6da0266d712a8470
Finnegan's Owner <44065187+pacmano1@users.noreply.github.com>
2026-03-11 14:21:56 -0500

    Bump version to 2.0.1-a

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -427,7 +427,7 @@ const deselectAll = () => {
             </button>
           </div>
           <div class="space-y-3 text-sm">
-            <p class="text-text-secondary">Version 2.0.0</p>
+            <p class="text-text-secondary">Version 2.0.1a</p>
             <div class="space-y-1">
               <p class="text-text-secondary">Originally created by <span class="text-text-primary">Kiran Ayyagari</span></p>
               <p class="text-text-secondary">Modifications by <span class="text-text-primary">Diridium Technologies Inc.</span></p>
